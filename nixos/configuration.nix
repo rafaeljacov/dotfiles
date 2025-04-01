84253c5d2acad1f554259ee490c31449625a97cf
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-01 19:41:05 +0800

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,7 +10,7 @@
   imports = let
     aagl-gtk-on-nix = import (builtins.fetchTarball {
       url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-      sha256 = "0z9lg60k9f58asx6myz25ysp3finfa8yrzz6ars2lasi5zhvg4s9";
+      sha256 = "1z8z53p7qd4mfawi098dls7mx52ax2cvygn3bd20n1va1l4zsx0z";
     });
   in [
     # Include the results of the hardware scan.
