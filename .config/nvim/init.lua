9d8e94058686e7e5310bcaf8d8b3a1a572cfc703
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-21 23:49:15 +0800

    remove print debug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,9 +34,8 @@ vim.cmd([[
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
     pattern = { "*.hl", "hypr*.conf" },
     callback = function(event)
-        print(string.format("starting hyprls for %s", vim.inspect(event)))
         vim.lsp.start {
-            name = "hyprlang",
+            name = "hyprls",
             cmd = { "hyprls" },
             root_dir = vim.fn.getcwd(),
         }
