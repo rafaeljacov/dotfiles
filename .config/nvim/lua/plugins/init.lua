71e78995497f0ae9e1fdcc6b7b8c62022d1397f0
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-21 23:18:54 +0800

    move hex plugin to init

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,12 +4,13 @@ return {
     'tpope/vim-fugitive',
     'nanotee/zoxide.vim',
     'windwp/nvim-ts-autotag',
+    { 'RaafatTurki/hex.nvim',    opts = {} },
     { 'lewis6991/gitsigns.nvim', opts = {} },
     { 'numToStr/Comment.nvim',   opts = {} },
     {
         "rachartier/tiny-inline-diagnostic.nvim",
         event = "VeryLazy", -- Or `LspAttach`
-        priority = 1000, -- needs to be loaded in first
+        priority = 1000,    -- needs to be loaded in first
         config = function()
             require('tiny-inline-diagnostic').setup()
             vim.diagnostic.config({ virtual_text = false })
