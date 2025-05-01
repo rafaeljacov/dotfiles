1f1c58bc67b18b7f5e8f1ef12fea8f21d37fb933
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-05-01 10:03:07 +0800

    follow nixpkgs input for zen

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
